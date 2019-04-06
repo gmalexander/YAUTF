03bbd9d7c16c4bce7f098d264485b7351607ba09
Grant Alexander <grant.m.alexander@hotmail.com>
2019-04-06 16:00:53 -0500

    Rename yautf.hh/cc -> TestManager.hh/cc, create new ones that are transitional for static linking/dynamic linking

--- yautf.cc
+++ yautf.cc
@@ -1,63 +1,9 @@
 #include "yautf.hh"
 
-/**************************************************************************************************
-*    YAUTF, Yet Another Unit Testing Framework. A microframework for quick C++ unit testing.      *
-*    Copyright (C) 2019  Grant Alexander                                                          *
-*                                                                                                 *
-*    This program is free software: you can redistribute it and/or modify                         *
-*    it under the terms of the GNU General Public License as published by                         *
-*    the Free Software Foundation, either version 3 of the License, or                            *
-*    (at your option) any later version.                                                          *
-*                                                                                                 *
-*    This program is distributed in the hope that it will be useful,                              *
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of                               *
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                *
-*    GNU General Public License for more details.                                                 *
-*                                                                                                 *
-*    You should have received a copy of the GNU General Public License                            *
-*    along with this program.  If not, see <https://www.gnu.org/licenses/>.                       *
-***************************************************************************************************/
-
-using namespace yautf;
-
-template <typename T>
-Yautf<T>::Yautf()
-{
-    this->Tests = new std::vector<Test<T>>();
-}
-
-template <typename T>
-Yautf<T>::~Yautf()
-{
-    auto start = this->Tests->begin();
-    auto end = this->Tests->end();
-    this->Tests->erase(start, end);
-    delete this->Tests;
-}
-
-template <typename T>
-void Yautf<T>::LoadTest(Test<T>* tst)
-{
-    this->Tests->push_back(*tst);
-}
-
-template <typename T>
-void Yautf<T>::VerifyTests()
-{
-    for(auto it = this->Tests->begin(); it != this->Tests->end(); it++)
-    {
-        it->Assert();
-    }
-}
-
-template <typename T>
-void Yautf<T>::DisplayAllResults()
-{
-    std::cout << "TEST RESULTS:" << "\n";
-    for(auto it = this->Tests->begin(); it != this->Tests->end(); it++)
-    {
-        std::cout << "\n";
-        it->DisplayResult();
-        std::cout << "\n\n";
-    }
-}
+template class TestManager<int>;
+template class TestManager<char>;
+template class TestManager<std::string>;
+template class TestManager<long>;
+template class TestManager<bool>;
+template class TestManager<double>;
+template class TestManager<float>;
