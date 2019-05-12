0dd5ef4bc3ce15b4b9fc8c188d0ca428b76c8170
gmalexander <grant.m.alexander@hotmail.com>
2019-05-12 10:41:12 -0500

    Update licensing from GPL v3 to LGPL v3 for projects linking to electron or node.

--- src/yautf_test.cc
+++ src/yautf_test.cc
@@ -1,5 +1,22 @@
 #include "yautf.hh"
 
+/**************************************************************************************************
+*    YAUTF, Yet Another Unit Testing Framework. A microframework for quick C++ unit testing.      *
+*    Copyright (C) 2019  Grant Alexander                                                          *
+*                                                                                                 *
+*    This program is free software: you can redistribute it and/or modify                         *
+*    it under the terms of the GNU Lesser General Public License as published by                  *
+*    the Free Software Foundation, either version 3 of the License, or                            *
+*    (at your option) any later version.                                                          *
+*                                                                                                 *
+*    This program is distributed in the hope that it will be useful,                              *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of                               *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                *
+*    GNU General Public License for more details.                                                 *
+*                                                                                                 *
+*    You should have received a copy of the GNU General Public License                            *
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.                       *
+***************************************************************************************************/
 template <typename T>
 void testInstantiationForType(T actual, T expected, std::string name)
 {
